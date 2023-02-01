ac6f3e2947f331ac98ac547ae6399befb1a8cd2d
raven4ever <wrtv23@gmail.com>
2023-02-01 14:33:44 +0200

    use basename for uploaded objects

--- infrastructure/content.tf
+++ infrastructure/content.tf
@@ -3,7 +3,7 @@ resource "aws_s3_object" "storage_bucket_demo_content" {
   for_each = fileset(path.module, format("%s/*", var.frontend_artefacts_location))
 
   bucket = aws_s3_bucket.storage_bucket.id
-  key    = each.value
+  key    = basename(each.value)
   source = each.value
   etag   = filemd5(each.value)
 }
