8b5ba2f3af1e876f7bd9586c3ec10022c3570751
raven4ever <wrtv23@gmail.com>
2023-02-01 14:41:15 +0200

    woff mimes

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,13 +1,15 @@
 locals {
   mime_types = {
-    "css"  = "text/css"
-    "html" = "text/html"
-    "ico"  = "image/vnd.microsoft.icon"
-    "js"   = "application/javascript"
-    "json" = "application/json"
-    "map"  = "application/json"
-    "png"  = "image/png"
-    "svg"  = "image/svg+xml"
-    "txt"  = "text/plain"
+    "css"   = "text/css"
+    "html"  = "text/html"
+    "ico"   = "image/vnd.microsoft.icon"
+    "js"    = "application/javascript"
+    "json"  = "application/json"
+    "map"   = "application/json"
+    "png"   = "image/png"
+    "svg"   = "image/svg+xml"
+    "txt"   = "text/plain"
+    "woff"  = "font/woff"
+    "woff2" = "font/woff2"
   }
 }
