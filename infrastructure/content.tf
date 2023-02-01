98fae6c4da67c0e22299d84eb497fc6918ac9588
raven4ever <wrtv23@gmail.com>
2023-02-01 14:38:51 +0200

    add content_type

--- infrastructure/content.tf
+++ infrastructure/content.tf
@@ -2,8 +2,9 @@
 resource "aws_s3_object" "storage_bucket_demo_content" {
   for_each = fileset(path.module, format("%s/*", var.frontend_artefacts_location))
 
-  bucket = aws_s3_bucket.storage_bucket.id
-  key    = basename(each.value)
-  source = each.value
-  etag   = filemd5(each.value)
+  bucket       = aws_s3_bucket.storage_bucket.id
+  key          = basename(each.value)
+  content_type = lookup(tomap(local.mime_types), element(split(".", each.value), length(split(".", each.value)) - 1))
+  source       = each.value
+  etag         = filemd5(each.value)
 }
