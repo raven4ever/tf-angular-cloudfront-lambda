8f8364a31828e243e01c4d8fe9babc7abce36a27
raven4ever <wrtv23@gmail.com>
2023-02-01 17:17:09 +0200

    typo

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_function" "backend_lambda" {
   filename      = "${path.module}/lambda.zip"
   function_name = "SuperDuperUberLambda"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "index.lambda_handler"
+  handler       = "main.lambda_handler"
   runtime       = "python3.9"
 }
