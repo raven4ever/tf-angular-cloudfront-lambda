400203e56d6b872b09b43dd347b2ca2218e18f07
raven4ever <wrtv23@gmail.com>
2023-02-01 14:15:04 +0200

    use path.module

--- infrastructure/content.tf
+++ infrastructure/content.tf
@@ -1,9 +1,9 @@
 # Upload demo content
 resource "aws_s3_object" "storage_bucket_demo_content" {
-  for_each = fileset("../frontend", "dist/frontend/*")
+  for_each = fileset("${path.module}../frontend/dist/frontend/", "*")
 
   bucket = aws_s3_bucket.storage_bucket.id
   key    = each.value
-  source = "../frontend/${each.value.source_path}"
-  etag   = filemd5("../frontend/${each.value.source_path}")
+  source = "${path.module}/${each.value.source_path}"
+  etag   = filemd5("${path.module}/${each.value.source_path}")
 }
