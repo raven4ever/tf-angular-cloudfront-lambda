fee4f774d49dae915fb8c2cedece95162482b2f5
raven4ever <wrtv23@gmail.com>
2023-02-01 11:21:57 +0200

    simple example

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { TableModule } from 'primeng/table';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    TableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
