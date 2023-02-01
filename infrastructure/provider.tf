ce7878bfcd1758fd18932185fc8ef041ef34a4b2
raven4ever <wrtv23@gmail.com>
2023-02-01 13:58:51 +0200

    remove unused

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -7,11 +7,6 @@ terraform {
       version = "~> 4.0"
     }
 
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2.0"
-    }
-
     local = {
       source  = "hashicorp/local"
       version = "~> 2.0"
