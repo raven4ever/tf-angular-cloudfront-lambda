9e78a843c56066f7b00a6f901b540e4e41f84eba
raven4ever <wrtv23@gmail.com>
2023-02-01 17:46:08 +0200

    add depends_on

--- infrastructure/build.tf
+++ infrastructure/build.tf
@@ -3,4 +3,8 @@ resource "null_resource" "build_frontend" {
     working_dir = "../frontend"
     command     = format("export NG_APP_API_URL=%s; ng build", locals.backend_url)
   }
+
+  depends_on = [
+    aws_apigatewayv2_stage.apigw_stage
+  ]
 }
