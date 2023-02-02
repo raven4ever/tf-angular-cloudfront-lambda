2e60ef9ed032ec0f97e1ca0552a90f4de42d886d
raven4ever <wrtv23@gmail.com>
2023-02-02 09:40:34 +0200

    trigger build when frontend changed

--- infrastructure/build.tf
+++ infrastructure/build.tf
@@ -1,7 +1,17 @@
+data "archive_file" "frontend_zip" {
+  type        = "zip"
+  source_dir  = "../frontend"
+  output_path = "frontend.zip"
+}
+
 resource "null_resource" "build_frontend" {
   provisioner "local-exec" {
     working_dir = "../frontend"
     command     = format("export NG_APP_API_URL=%s; npm i; ng build", local.backend_url)
+  }
+
+  triggers = {
+    frontend_hash = data.archive_file.frontend_zip.output_sha
   }
 
   depends_on = [
