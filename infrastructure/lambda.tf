3574f0593893d8b897b6c9345bbbadafd5cb7529
raven4ever <wrtv23@gmail.com>
2023-02-02 16:10:17 +0200

    adding neptune utils layer

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -4,12 +4,19 @@ data "archive_file" "lambda_zip" {
   output_path = "${path.module}/lambda.zip"
 }
 
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename            = "./lambda_layers/neptune_tools_builder/target/neptune_python_utils_lambda_layer.zip"
+  layer_name          = "lambda_layer_name"
+  compatible_runtimes = ["python3.8"]
+}
+
 resource "aws_lambda_function" "backend_lambda" {
   filename      = data.archive_file.lambda_zip.output_path
   function_name = var.lambda_function_name
   role          = aws_iam_role.lambda_role.arn
   handler       = "main.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.8"
+  layers        = [aws_lambda_layer_version.lambda_layer.arn]
 
   environment {
     variables = {
