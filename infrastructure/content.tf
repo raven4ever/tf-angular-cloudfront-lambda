c66254ad41b71845f1d49e899bfd3164e489ff6f
raven4ever <wrtv23@gmail.com>
2023-02-03 18:40:13 +0200

    use each.key

--- infrastructure/content.tf
+++ infrastructure/content.tf
@@ -3,10 +3,10 @@ resource "aws_s3_object" "website_bucket_demo_content" {
   for_each = fileset(path.module, format("%s/*", var.frontend_artefacts_location))
 
   bucket       = aws_s3_bucket.website_bucket.id
-  key          = basename(each.value)
-  content_type = lookup(tomap(local.mime_types), element(split(".", each.value), length(split(".", each.value)) - 1))
-  source       = each.value
-  etag         = filemd5(each.value)
+  key          = basename(each.key)
+  content_type = lookup(tomap(local.mime_types), element(split(".", each.key), length(split(".", each.key)) - 1))
+  source       = each.key
+  etag         = filemd5(each.key)
 
   depends_on = [
     null_resource.build_frontend
